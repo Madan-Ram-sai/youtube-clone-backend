5b870cb8a3c7313b4e26e8cf6acda0642b569425
madan-ram-sai <vmadanramsai@gmail.com>
2026-04-05 20:07:55 +0530

    cloudnary and multer setup setup completed here

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,18 @@
+import multer from "multer";
+
+// const storage = multer.memoryStorage() // store file in memory as buffer
+const storage = multer.diskStorage({
+    destination: function(req, file,cb){// cb==call back function
+        cb(null,"./public/temp")
+    },
+    filename: function(req, file,cb){
+        // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random()*1E9)// this is for unique file name to avoid overwriting of files with same name
+        // const ext = file.originalname.split(".").pop()// get file extension
+        // cb(null, file.fieldname + "-" + uniqueSuffix + "." + ext)
+
+        // if we want to keep it simple
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({storage: storage})
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+// this is for hadling file upload to cloudinary
+// this is first way 2 step process first uploaded to our server and then moved to cloudinary
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs" // file system module of node js to handle files(here unlink means delete )
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+const uploadOnCloudinary = async (localFilePath)=>{
+    try{
+        if(!localFilePath) return null
+        // upload file on cloudinary and get the url 
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto",
+        })
+        // file uploaded successfully 
+        console.log("file uploaded on cloudinary successfully", response, "file url", response.url)
+        return response;
+
+    }catch(error){
+        console.log("error while uploading file on cloudinary", error)
+        fs.unlinkSync(localFilePath) // delete the file from local storage if error occurs while uploading on cloudinary
+        return null
+    }
+}
+
+export { uploadOnCloudinary}
